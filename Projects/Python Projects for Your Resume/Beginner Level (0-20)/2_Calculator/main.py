6a7348fea3fccf2fd6f353f5bb3f3a104b421b63
ramin_yz <yazdani76ramin@gmail.com>
2023-09-25 21:19:49 +0330

    changing directory modfied paths

---
+++ Projects/Python Projects for Your Resume/Beginner Level (0-20)/2_Calculator/main.py
@@ -0,0 +1,25 @@
+# Function to perform addition
+def add(x, y):
+    pass
+
+# Function to perform subtraction
+def subtract(x, y):
+    pass
+
+# Function to perform multiplication
+def multiply(x, y):
+    pass
+
+# Function to perform division
+def divide(x, y):
+    pass
+
+# Function to perform if the input is a number
+def is_number(*args):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    pass
